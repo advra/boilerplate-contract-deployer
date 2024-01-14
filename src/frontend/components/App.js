d5d86cb5639e852a15abce7ba9947abd6687d4e6
advra <enthusiasticnftartist@gmail.com>
2024-01-14 01:26:48 -0800

    Added contract abi modal

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,41 +1,18 @@
-// import React, { useState, useEffect } from 'react';
-// import './App.css';
-
-// import AbstractSRNFTAbi from '../contractsData/AbstractSRNFT.json'
-// import AbstractSRNFTeAddress from '../contractsData/AbstractSRNFT-address.json'
-
-// function App() {
-//   const [contractAddress, setContractAddress] = useState('');
-//   // const [contractABI, setContractABI] = useState([]);
-
-//   useEffect(() => {
-
-//     setContractAddress(AbstractSRNFTeAddress.address);
-
-
-//     // If you need to fetch the ABI as well, you can add another fetch call here
-
-//   }, []); // Empty dependency array to ensure this effect runs only once on mount
-
-//   return (
-//     <div className="App">
-//       <div>Boilerplate Contract Deployer</div>
-//       <div>This is a minimal web example:</div>
-//       <div>Contract Address: {contractAddress} </div>
-//       {/* <div>Contract ABI: {JSON.stringify(contractABI, null, 2)} </div> */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useState, useEffect } from 'react';
 import { Button, TextField, Modal, Box, Typography, IconButton } from '@mui/material';
+import ContractAbiModal from './ContractAbiModal';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import AbstractSRNFTAbi from '../contractsData/AbstractSRNFT.json'
 import AbstractSRNFTeAddress from '../contractsData/AbstractSRNFT-address.json'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -56,19 +33,6 @@ function App() {
   const incrementMintAmount = () => setMintAmount(mintAmount + 1);
   const decrementMintAmount = () => mintAmount > 0 && setMintAmount(mintAmount - 1);
 
-  const modalStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 950,
-    maxHeight: '80vh',
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    p: 4,
-    overflow: 'hidden' // This ensures that any overflow is within the TextField
-  };
-
   // // Dummy ABI data
   // const abiData = JSON.stringify(MarketplaceAbi.abi, null, 2);
   useEffect(() => {
@@ -78,89 +42,65 @@ function App() {
   }, []); // Empty dependency array to ensure this effect runs only once on mount
 
     return (
-      <div className="App" style={{ 
-          textAlign: 'center', 
-          maxWidth: 550, 
-          margin: 'auto', 
-          paddingTop:'10vh' 
-      }}>
-        <Typography variant="h4">
-          Boilerplate Contract Deployer
-        </Typography>
-        <Box
-          marginTop={8}
-          marginBottom={2}
-          display="flex"
-          alignItems="center" // Align items vertically
-          gap={1} // Spacing between items
-        >
-          <Typography variant='h6'>Contract Deployed:</Typography>
-          <TextField
-            value={contractAddress}
-            InputProps={{ readOnly: true }}
-            variant="outlined"
-            style={{ marginRight: 8 }}
-          />
-          <Button 
-          style={{ height: '55px' }}
-            variant="outlined" 
-            onClick={handleOpen}>
-              Check ABI
-          </Button>
-        </Box>
-        <Box>
-          <IconButton onClick={decrementMintAmount} disabled={mintAmount <= 0}>
-            <RemoveIcon />
-            </IconButton>
-        <TextField
-          value={mintAmount}
-          InputProps={{ readOnly: true }}
-          style={{ width: 50, textAlign: 'center' }}
-        />
-        <IconButton onClick={incrementMintAmount}>
-          <AddIcon />
-        </IconButton>
-      </Box>
-      <Button 
-        size="large"
-        variant="contained" 
-        color="primary" 
-        onClick={handleMint} 
-        style={{ marginTop: 16 }}>
-        Mint
-      </Button>
-
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-title"
-        aria-describedby="modal-description"
-      >
-        <Box sx={modalStyle}>
-          <Typography id="modal-title" variant="h6" component="h2">
-            Contract ABI
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <div className="App" style={{ 
+            textAlign: 'center', 
+            maxWidth: 650, 
+            margin: 'auto', 
+            paddingTop:'10vh' 
+        }}>
+          <Typography variant="h4">
+            Boilerplate Contract Deployer
           </Typography>
+          <Box
+            marginTop={8}
+            marginBottom={2}
+            display="flex"
+            alignItems="center" // Align items vertically
+            gap={1} // Spacing between items
+          >
+            <Typography variant='h6'>Contract:</Typography>
+            <TextField
+              value={contractAddress}
+              InputProps={{ readOnly: true }}
+              variant="outlined"
+              style={{ 
+                marginRight: 8,
+                width: '100%',
+              }}
+            />
+            <Button 
+            style={{ height: '55px' }}
+              variant="outlined" 
+              onClick={handleOpen}>
+                Check ABI
+            </Button>
+          </Box>
+          <Box>
+            <IconButton onClick={decrementMintAmount} disabled={mintAmount <= 0}>
+              <RemoveIcon />
+              </IconButton>
           <TextField
-            id="modal-description"
-            multiline
-            fullWidth
-            label="JSON Preview"
-            value={contractAbi}
+            value={mintAmount}
             InputProps={{ readOnly: true }}
-            variant="outlined"
-            margin="normal"
-            sx={{
-              height: '500px', // Subtract the padding from the height
-               // Re-enable vertical scrolling
-              '&& .MuiInputBase-inputMultiline': {
-                padding: '10px', // Padding inside the TextField
-                // overflowY: 'auto',
-              }
-            }}
+            style={{ width: 50, textAlign: 'center' }}
           />
+          <IconButton onClick={incrementMintAmount}>
+            <AddIcon />
+          </IconButton>
         </Box>
-      </Modal>
-    </div>
+        <Button 
+          size="large"
+          variant="contained" 
+          color="primary" 
+          onClick={handleMint} 
+          style={{ marginTop: 16 }}>
+          Mint
+        </Button>
+        <ContractAbiModal open={open} close={handleClose} json={contractAbi} />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/frontend/components/ContractAbiModal.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Button, Modal, Box, Typography, TextField } from '@mui/material';
+
+const ContractAbiModal = ({ open, close, json }) => {
+  return (
+    <Modal open={open} onClose={close}>
+      <Box sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 800,
+        height: '80vh',
+        bgcolor: 'background.paper',
+        boxShadow: 24,
+        p: 4,
+        overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column', // Stack children vertically
+      }}>
+        <div style={{ flex: '1 1 auto' }}>
+          <Typography variant="h6" component="h2">
+            Contract ABI
+          </Typography>
+          <TextField
+            multiline
+            rows={35}
+            variant="outlined"
+            fullWidth
+            value={json}
+            sx={{
+              flex: '1 1 auto', // Makes the TextField flexible
+              '& .MuiInputBase-root': {
+                padding: '25px',
+                height: '100%', // TextField takes the full height
+              },
+              '& .MuiInputBase-inputMultiline': {
+                height: '100%', // Ensures the inner part takes full height
+              },
+            }}
+          />
+        </div>
+        <Button 
+          onClick={close} 
+          sx={{ mt: 2, alignSelf: 'flex-end' }} // Aligns the button to the right
+        >
+          Close
+        </Button>
+      </Box>
+    </Modal>
+  );
+};
+
+
+export default ContractAbiModal;
